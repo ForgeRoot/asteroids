c726a049bdc68d67ca7e9440c5517d28f889759d
ForgeRoot <dylananne.v.eck@gmail.com>
2025-09-22 18:05:05 +0200

    used groups instead of individuals

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -14,10 +17,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        for draw_object in drawable:
+            draw_object.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60)/1.000
